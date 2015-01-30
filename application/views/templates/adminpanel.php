303e6670756beeeac2e8cad825a6e07855aaa007
Isaac Cacho G <msn@swfideas.com>
2015-01-30 06:01:02 -0600

    2
    2

--- application/views/templates/adminpanel.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$this->load->view('includes/adminheader.php');
-
-$this->load->view('includes/adminmenu.php');
-
-$this->load->view($main_content);
-
-$this->load->view('includes/adminfooter.php');
